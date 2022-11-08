d653a048d764f3e0ed0eef2cbb97094bf5772263
Haroun Anwari <harounanwari@hotmail.co.uk>
2022-11-08 15:43:23 +0000

    core done

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-import Header from './components/header'
+import { useState } from "react";
+import Header from "./components/header";
 
-import initialEmails from './data/emails'
+import initialEmails from "./data/emails";
 
-import './styles/app.css'
+import "./styles/app.css";
 
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails);
+  console.log(initialEmails);
 
   return (
     <div className="app">
@@ -39,9 +40,27 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {/* Render a list of emails here */}
+        {emails.map((email, index) => (
+          <li className="email" key={index}>
+            <div className="select">
+              <input className="select-checkbox" type="checkbox" />
+            </div>
+            <div className="star">
+              <input
+                className="star-checkbox"
+                type="checkbox"
+                checked={email.starred}
+              />
+            </div>
+            <div className="sender"> {email.sender}</div>
+            <div className="title">{email.title}</div>
+          </li>
+        ))}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
